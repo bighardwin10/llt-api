80639ce9e59238d53a64a5e5fcd22f88a1f99918
BigHardWin10 <win_tree@163.com>
2026-03-29 15:51:08 +0800

    LICENSE and more

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import { Hono } from "hono/quick"
+const app = new Hono()
+import { cors } from 'hono/cors'
+
+const apiVer = "v1"
+
+app.use("/*",cors({
+	origin: "*",
+	allowMethods: ['GET', 'POST']
+}))
+
+class TemplateResp{
+	constructor(status,message,data){
+		this.status = status
+		this.message = message
+		this.data = data
+	}
+	dump() {
+		let temp = {
+			"status": this.status,
+			"message": this.message,
+			"data": this.data
+		}
+		return temp
+	}
+}
+
+app.get('/', (c) => {
+	const resp = new TemplateResp(200,"服务正常运行",null)
+	return c.json(resp.dump())
+}).get(`/${apiVer}`, (c) => {
+	const ua = c.req.header('User-Agent')
+	var pattern = new RegExp("^LimbusLocalizeTool/[0-9]\.[0-9]\.[0-9]$")
+	if(!pattern.test(ua)){
+		const resp = new TemplateResp(403,"非客户端请求",null)
+		c.status(403)
+		return c.json(resp.dump())
+	}
+	const major = c.env.LLT.get("VERSION_MAJOR")
+	const minor = c.env.LLT.get("VERSION_MINOR")
+	const patch = c.env.LLT.get("VERSION_PATCH")
+	const resp = new TemplateResp(200,"成功",{"major": major,"minor": minor,"patch": patch})
+	return c.json(resp.dump())
+})
+
+export default app
